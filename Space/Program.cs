ef1d6563f8d098a49d43c0eceff2629e727ea2fc
Nilovva <slavka11118@yandex.ru>
2024-01-17 22:21:01 +0400

    Добавьте файлы проекта.

---
+++ Space/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Space
+{
+    public static void Main()
+    {
+        string aString = "Just like William Shakespeare on literature, and Sigmund Freud on psychology, Picasso's impact on art is tremendous.";
+        IEnumerable<char> stringQuery2 = aString.Where(e => e == ' ');
+
+        Console.Write(stringQuery2.Count());
+
+        Console.WriteLine(System.Environment.NewLine + "Press any key to exit");
+        Console.ReadLine();
+    }
+}
